829267d197b7f3ebcb465535d86f6e46c2c989b6
vinicius <https://github.com/deivios>
2025-11-12 16:26:16 -0300

    Aula inteiros

---
+++ lib/tipos/inteiros.dart
@@ -0,0 +1,17 @@
+void main() {
+  int altura = 183;
+
+  print("Altura: $altura cm");
+
+  int ano = 2025;
+
+  print("Ano: $ano");
+
+  int idade = 27;
+
+  print("Idade: $idade");
+
+  int dataNascimento = ano - idade;
+
+  print("Data nascimento: $dataNascimento");
+}
