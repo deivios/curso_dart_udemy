cd43ae87bbbc8b4304872e70582b98fe08a5d640
vinicius <https://github.com/deivios>
2025-11-12 16:13:54 -0300

    Aula variaveis

---
+++ lib/tipos/variaveis.dart
@@ -0,0 +1,36 @@
+void main() {
+  var nome  ="Paulo";
+
+  nome = "Paulo Mendes";
+
+  print(nome);
+
+  var idade  = 27;
+
+  print(idade);
+
+  var peso = 72.5;
+
+  peso = 70;
+
+  print(peso);
+
+  
+  var maiorIdade = true;
+
+  print(maiorIdade);
+
+  const nomeCompleto = "Paulo Antônio Ferreira Mendes Macêdo";
+
+  print(nomeCompleto);
+
+  final int altura; 
+
+  altura = 80;
+
+  late String nomePai;
+
+  nomePai = "José";
+
+  print(nomePai);
+}
